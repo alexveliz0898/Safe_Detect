a0a7af4bfa8ee7f875a28739d50103e398a8bef1
alexveliz0898 <alexveliz0898@gmail.com>
2019-01-27 17:10:06 -0500

    Update twilio_code.py

--- twilio_code.py
+++ twilio_code.py
@@ -5,13 +5,13 @@ import os
 # need auth_token
 def sendMessage(text):
     account_sid = "ACa713ce4b1075bc4e7aa84f6ed3656303"
-    auth_token = "863e8136971af33e2b8a83e96ab103b3"
-    twilio_Number = '+13478366081'
+    auth_token = "****************************" 
+    twilio_Number = '+134783****'
 
     client = Client(account_sid, auth_token)
 
     #our list of numbers
-    numbers = ['+19172080459', '+13477337209', '+13472579022', '+15165068101', '+16315756224', '+13474185648']
+    numbers = ['+1917208****', '+1347733****', '+1347257****', '+151650****', '+1631575****', '+1347418****']
 
     for person_number in list(numbers):
         message = client.messages.create(
